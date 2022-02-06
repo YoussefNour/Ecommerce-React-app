4d2c0a0b8c5e6ef082ec33aaee802195acab7127
YoussefNour <youssef.ossama2@gmail.com>
2022-02-06 12:46:59 +0200

    Adds actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,13 @@
 import { combineReducers } from "redux";
+import { CreateNewProduct } from "./Products/Create-Product";
+import { GetProducts } from "./Products/Get-Products";
+import { GetProductId } from "./Products/Get-Products-By-Id";
 
-const rootReducer = combineReducers({});
 
-export default rootReducer;
+let rootReducers = combineReducers({
+    productList:GetProducts,
+    GetProduct : GetProductId,
+    CreateProduct : CreateNewProduct
+})
+
+export default rootReducers
