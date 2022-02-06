2566e5b011b44f3ffee4c899504cb3acace121c9
Gwailyyy <ahmedmfarag@gmail.com>
2022-02-06 17:14:54 +0200

    login

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,15 +3,13 @@ import { CreateNewProduct } from "./Products/Create-Product";
 import { GetProducts } from "./Products/Get-Products";
 import { GetProductId } from "./Products/Get-Products-By-Id";
 import { CreateNewUser } from "./Users/Create-User";
-import { GetUsers } from "./Users/Get-Users";
 
 
 let rootReducers = combineReducers({
     productList:GetProducts,
     GetProduct : GetProductId,
     CreateProduct : CreateNewProduct,
-    CreateUser : CreateNewUser,
-    usersList : GetUsers
+    CreateUser : CreateNewUser
 })
 
 export default rootReducers
